af650fd588d7c0e0f04d6c61dbd9b8d9642fda9f
Mallow-x <mallory.scotton@epitech.eu>
2024-01-09 19:33:36 +0100

    ✨ Adding Bezier Curve

---
+++ include/SFML/Addition/BezierCurve.h
@@ -0,0 +1,98 @@
+////////////////////////////////////////////////////////////
+//
+// CSFML-ADDITION - Simple and Fast Multimedia Library addon
+// Copyright (C) 2023 Mallory Scotton (mscotton.pro@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef SFML_BEZIERCURVE_H
+    #define SFML_BEZIERCURVE_H
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics.h>
+#include <stdlib.h>
+
+////////////////////////////////////////////////////////////
+/// \brief
+///
+////////////////////////////////////////////////////////////
+typedef struct
+{
+    sfVector2f *points;
+    size_t pointCount;
+    sfColor color;
+} sfBezierCurve;
+
+////////////////////////////////////////////////////////////
+/// \brief Construct a new bezier curve
+///
+////////////////////////////////////////////////////////////
+CSFML_API_EXPORT sfBezierCurve *sfBezierCurve_create(void);
+
+////////////////////////////////////////////////////////////
+/// \brief Destroy an existing bezier curve
+///
+/// \param bezierCurve Bezier curve to destroy
+///
+////////////////////////////////////////////////////////////
+CSFML_API_EXPORT void sfBezierCurve_destroy(sfBezierCurve *bezierCurve);
+
+////////////////////////////////////////////////////////////
+/// \brief Set the color of the curve
+///
+/// By default, the color is set to sfTransparent.
+///
+/// \param bezierCurve  Bezier curve to object
+/// \param color        New color of the curve
+///
+////////////////////////////////////////////////////////////
+CSFML_API_EXPORT void sfBezierCurve_setCurveColor(sfBezierCurve *bezierCurve, sfColor color);
+
+////////////////////////////////////////////////////////////
+/// \brief Add a new point to the curve
+///
+/// \param bezierCurve  Bezier curve to object
+/// \param point        New point of the curve
+///
+////////////////////////////////////////////////////////////
+CSFML_API_EXPORT void sfBezierCurve_addPoint(sfBezierCurve *bezierCurve, sfVector2f point);
+
+////////////////////////////////////////////////////////////
+/// \brief Calculate current point based with time in percent from 0 to 1
+///
+/// \param bezierCurve  Bezier curve to object
+/// \param time         Time from 0 to 1 for point offset
+///
+////////////////////////////////////////////////////////////
+CSFML_API_EXPORT sfVector2f sfBezierCurve_calculatePoint(const sfBezierCurve *bezierCurve, float time);
+
+////////////////////////////////////////////////////////////
+/// \brief Draw a drawable object to the render-target
+///
+/// \param renderWindow render window object
+/// \param bezierCurve  Object to draw
+/// \param states       Render states to use for drawing (NULL to use the default states)
+///
+////////////////////////////////////////////////////////////
+CSFML_API_EXPORT void sfRenderWindow_drawBezierCurve(const sfRenderWindow *renderWindow, const sfBezierCurve *bezierCurve, const sfRenderStates *states);
+
+#endif // SFML_BEZIERCURVE_H
---
+++ source/BezierCurve.c
@@ -0,0 +1,108 @@
+////////////////////////////////////////////////////////////
+//
+// CSFML-ADDITION - Simple and Fast Multimedia Library addon
+// Copyright (C) 2023 Mallory Scotton (mscotton.pro@gmail.com)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Addition/BezierCurve.h>
+
+////////////////////////////////////////////////////////////
+sfBezierCurve *sfBezierCurve_create(void)
+{
+    sfBezierCurve *bezierCurve = malloc(sizeof(sfBezierCurve));
+
+    if (!bezierCurve)
+        return (bezierCurve);
+    bezierCurve->color = sfTransparent;
+    bezierCurve->pointCount = 0;
+    bezierCurve->points = NULL;
+    return (bezierCurve);
+}
+
+////////////////////////////////////////////////////////////
+void sfBezierCurve_destroy(sfBezierCurve *bezierCurve)
+{
+    if (bezierCurve == NULL)
+        return;
+    free(bezierCurve->points);
+    free(bezierCurve);
+}
+
+////////////////////////////////////////////////////////////
+void sfBezierCurve_setCurveColor(sfBezierCurve *bezierCurve, sfColor color)
+{
+    if (bezierCurve == NULL)
+        return;
+    bezierCurve->color = color;
+}
+
+////////////////////////////////////////////////////////////
+void sfBezierCurve_addPoint(sfBezierCurve *bezierCurve, sfVector2f point)
+{
+    if (bezierCurve == NULL)
+        return;
+    bezierCurve->pointCount++;
+    bezierCurve->points = realloc(bezierCurve->points, bezierCurve->pointCount * sizeof(sfVector2f));
+    bezierCurve->points[bezierCurve->pointCount - 1] = point;
+}
+
+////////////////////////////////////////////////////////////
+sfVector2f sfBezierCurve_calculatePoint(const sfBezierCurve *bezierCurve, float time)
+{
+    sfVector2f *points = bezierCurve->points;
+    size_t n = bezierCurve->pointCount - 1;
+    sfVector2f *tmpPoints = malloc(sizeof(sfVector2f) * bezierCurve->pointCount);
+    sfVector2f result;
+
+    if (n < 1)
+        return ((sfVector2f){-1, -1});
+    for (size_t i = 0; i < bezierCurve->pointCount; i++)
+        tmpPoints[i] = points[i];
+    for (size_t k = 0; k <= n; k++) {
+        for (size_t i = 0; i <= n; i++) {
+            tmpPoints[i].x = (1 - time) * tmpPoints[i].x + time * tmpPoints[i + 1].x;
+            tmpPoints[i].y = (1 - time) * tmpPoints[i].y + time * tmpPoints[i + 1].y;
+        }
+    }
+    result = tmpPoints[0];
+    free(tmpPoints);
+    return (result);
+}
+
+////////////////////////////////////////////////////////////
+void sfRenderWindow_drawBezierCurve(const sfRenderWindow *renderWindow, const sfBezierCurve *bezierCurve, const sfRenderStates *states)
+{
+    sfVertexArray *curve = sfVertexArray_create();
+    sfVertex vertex;
+
+    if (bezierCurve == NULL || bezierCurve->pointCount < 2)
+        return (sfVertexArray_destroy(curve));
+    for (float time = 0; time < 1; time += 0.0001) {
+        vertex.position = sfBezierCurve_calculatePoint(bezierCurve, time);
+        vertex.color = bezierCurve->color;
+        sfVertexArray_append(curve, vertex);
+    }
+    sfRenderWindow_drawVertexArray(renderWindow, curve, states);
+    sfVertexArray_destroy(curve);
+}
